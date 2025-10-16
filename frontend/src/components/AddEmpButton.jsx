9bf3cea5a3bb00e16442bf5c7f881a733a439a6f
codebylorence <rodriguezlorence4>
2025-10-16 18:12:13 +0800

    modified new components

--- frontend/src/components/AddEmpButton.jsx
+++ frontend/src/components/AddEmpButton.jsx
@@ -2,7 +2,7 @@ import { FaPlus } from "react-icons/fa6";
 
 export default function AddEmpButton() {
   return (
-    <button className="flex items-center justify-center gap-2 bg-[#1E3A8A] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded-md transition ">
+    <button className="flex items-center justify-center gap-2 bg-[#1E3A8A] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded-md transition cursor-pointer">
       <span>Add Employee</span>
       <div className="bg-white rounded-full p-1">
         <FaPlus color="#1E3A8A"/>
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar() {
         placeholder="Search Employee..."
         className="flex-1 px-3 py-2 text-sm text-gray-700 focus:outline-none"
       />
-      <button className="bg-gray-700 hover:bg-gray-800 text-white px-3 py-2 flex items-center justify-center">
+      <button className="bg-gray-700 hover:bg-gray-800 text-white px-3 py-2 flex items-center justify-center cursor-pointer">
         <IoSearchOutline size="25"/>
       </button>
     </div>
